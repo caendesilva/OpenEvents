022f480f4bbe17ed54beab9205ca1210609b7127
Caen De Silva <caen@desilva.se>
2022-04-22 23:42:42 +0200

    Move singleton binding to register method

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton('HumanoIDGenerator', function ($app) {
+            return new HumanoIDGenerator($app->make(HumanoIDGenerator::class));
+        });
     }
 
     /**
@@ -24,8 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->app->singleton('HumanoIDGenerator', function ($app) {
-            return new HumanoIDGenerator($app->make(HumanoIDGenerator::class));
-        });
+        //
     }
 }
