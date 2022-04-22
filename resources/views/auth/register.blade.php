7bc13754f2fbf5a9923651727b5604fdf6f5bf08
Caen De Silva <caen@desilva.se>
2022-04-22 21:05:06 +0200

    Update form label wording to reflect name belongs to project

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,8 +10,8 @@
             @csrf
 
             <div>
-                <x-jet-label for="name" value="{{ __('Name') }}" />
-                <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                <x-jet-label for="name" value="{{ __('Project Name') }}" />
+                <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus/>
             </div>
 
             <div class="mt-4">
