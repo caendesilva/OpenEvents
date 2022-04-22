8bafee6aaeb2c3ea150ffdcdbcc6c61159b652b2
Caen De Silva <caen@desilva.se>
2022-04-22 20:24:05 +0200

    Add FAQ to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,44 +1,188 @@
 <x-guest-layout>
-    <!-- Section 1 - Component by Tauls --> 
+    <style>
+        html {
+            scroll-behavior: smooth;
+        }
+    </style>
+    <!-- Section 1 - Component by Tails -->
     <section class="w-full px-6 pb-12 antialiased bg-white">
         <div class="">
             <nav class="relative z-50 h-24 select-none" x-data="{ showMenu: false }">
-                <div class="container relative flex flex-wrap items-center justify-between h-24 mx-auto overflow-hidden font-medium border-b border-gray-200 md:overflow-visible lg:justify-center sm:px-4 md:px-2">
-                    <div class="flex items-center justify-start w-1/4 h-full pr-4"> <a href="#_" class="inline-block py-4 md:py-0"> <span class="p-1 text-xl font-black leading-none text-gray-900"><span>{{ config('app.name') }}</span><span class="text-indigo-600">.</span></span> </a> </div>
-                    <div class="top-0 left-0 items-start hidden w-full h-full p-4 text-sm bg-gray-900 bg-opacity-50 md:items-center md:w-3/4 lg:text-base md:bg-transparent md:p-0 md:relative md:flex" :class="{'flex fixed': showMenu, 'hidden': !showMenu }">
-                        <div class="flex-col w-full h-auto overflow-hidden bg-white rounded-lg md:bg-transparent md:overflow-visible md:rounded-none md:relative md:flex md:flex-row">
-                            <a href="#_" class="inline-flex items-center w-auto h-16 px-6 text-xl font-black leading-none text-gray-900 md:hidden">tails<span class="text-indigo-600">.</span></a> 
-                            <div class="flex flex-col items-start justify-center w-full space-x-6 text-center lg:space-x-8 md:w-2/3 md:mt-0 md:flex-row md:items-center">
-                                <a href="#_" class="inline-block w-full py-2 mx-0 ml-6 font-medium text-left text-indigo-600 md:ml-0 md:w-auto md:px-0 md:mx-2 lg:mx-3 md:text-center">Home</a> <a href="#_" class="inline-block w-full py-2 mx-0 font-medium text-left text-gray-700 md:w-auto md:px-0 md:mx-2 hover:text-indigo-600 lg:mx-3 md:text-center">Dashboard</a> <a href="#_" class="inline-block w-full py-2 mx-0 font-medium text-left text-gray-700 md:w-auto md:px-0 md:mx-2 hover:text-indigo-600 lg:mx-3 md:text-center">Pricing</a> <a href="#_" class="inline-block w-full py-2 mx-0 font-medium text-left text-gray-700 md:w-auto md:px-0 md:mx-2 hover:text-indigo-600 lg:mx-3 md:text-center">About</a> 
-                                <a href="#_" class="absolute top-0 left-0 hidden py-2 mt-6 ml-10 mr-2 text-gray-600 lg:inline-block md:mt-0 md:ml-2 lg:mx-3 md:relative">
-                                    <svg class="inline w-5 h-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                                    </svg>
-                                </a>
-                            </div>
-                            <div class="flex flex-col items-start justify-end w-full pt-4 md:items-center md:w-1/3 md:flex-row md:py-0"> <a href="#" class="w-full px-6 py-2 mr-0 text-gray-700 md:px-0 lg:pl-2 md:mr-4 lg:mr-5 md:w-auto">Sign In</a> <a href="#_" class="inline-flex items-center w-full px-6 py-3 text-sm font-medium leading-4 text-white bg-indigo-600 md:px-3 md:w-auto md:rounded-full lg:px-5 hover:bg-indigo-500 focus:outline-none md:focus:ring-2 focus:ring-0 focus:ring-offset-2 focus:ring-indigo-600">Sign Up</a> </div>
+                <div
+                    class="container relative max-w-full flex flex-wrap items-center justify-between h-24 mx-auto overflow-hidden font-medium border-b border-gray-200 md:overflow-visible lg:justify-center sm:px-4 md:px-2">
+                    <div class="flex items-center justify-start w-1/4 h-full pr-4"> <a href="#_"
+                            class="inline-block py-4 md:py-0"> <span
+                                class="p-1 text-xl font-black leading-none text-gray-900"><span>{{ config('app.name')
+                                    }}</span><span class="text-indigo-600">.</span></span> </a> </div>
+                    <div class="top-0 left-0 items-start hidden w-full h-full p-4 text-sm bg-gray-900 bg-opacity-50 md:items-center md:w-3/4 lg:text-base md:bg-transparent md:p-0 md:relative md:flex"
+                        :class="{'flex fixed': showMenu, 'hidden': !showMenu }">
+                        <div
+                            class="flex-col w-full h-auto justify-end overflow-hidden bg-white rounded-lg md:bg-transparent md:overflow-visible md:rounded-none md:relative md:flex md:flex-row">
+                            <a href="#_"
+                                class="inline-flex items-center w-auto h-16 px-6 text-xl font-black leading-none text-gray-900 md:hidden">tails<span
+                                    class="text-indigo-600">.</span></a>
+                            <div
+                                class="flex flex-col items-start justify-end w-full space-x-6 text-center lg:space-x-8 md:w-2/3 md:mt-0 md:flex-row md:items-center">
+                                <a href="#_"
+                                    class="inline-block w-full py-2 mx-0 ml-6 font-medium text-left text-indigo-600 md:ml-0 md:w-auto md:px-0 md:mx-2 lg:mx-3 md:text-center">Home</a>
+                                <a href="#_"
+                                    class="inline-block w-full py-2 mx-0 font-medium text-left text-gray-700 md:w-auto md:px-0 md:mx-2 hover:text-indigo-600 lg:mx-3 md:text-center">Explore</a>
+                                    <a href="#_"
+                                    class="inline-block w-full py-2 mx-0 font-medium text-left text-gray-700 md:w-auto md:px-0 md:mx-2 hover:text-indigo-600 lg:mx-3 md:text-center">Dashboard</a>
+                               
+                                </div>
+                            <div
+                                class="flex flex-col items-start justify-end w-full pt-4 pl-8 md:items-center md:w-fit md:flex-row md:py-0">
+                                <a href="#"
+                                    class="w-full px-6 py-2 mr-0 text-gray-700 md:px-0 lg:pl-2 md:mr-4 lg:mr-5 md:w-auto">Sign
+                                    In</a> <a href="#_"
+                                    class="inline-flex items-center w-full px-6 py-3 text-sm font-medium leading-4 text-white bg-indigo-600 md:px-3 md:w-auto md:rounded-full lg:px-5 hover:bg-indigo-500 focus:outline-none md:focus:ring-2 focus:ring-0 focus:ring-offset-2 focus:ring-indigo-600">Sign
+                                    Up</a> </div>
                         </div>
                     </div>
-                    <div @click="showMenu = !showMenu" class="absolute right-0 flex flex-col items-center justify-center w-10 h-10 bg-white rounded-full cursor-pointer md:hidden hover:bg-gray-100">
-                        <svg class="w-6 h-6 text-gray-700" x-show="!showMenu" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" x-cloak="">
+                    <div @click="showMenu = !showMenu"
+                        class="absolute right-0 flex flex-col items-center justify-center w-10 h-10 bg-white rounded-full cursor-pointer md:hidden hover:bg-gray-100">
+                        <svg class="w-6 h-6 text-gray-700" x-show="!showMenu" fill="none" stroke-linecap="round"
+                            stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"
+                            x-cloak="">
                             <path d="M4 6h16M4 12h16M4 18h16"></path>
                         </svg>
-                        <svg class="w-6 h-6 text-gray-700" x-show="showMenu" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" x-cloak="">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                        <svg class="w-6 h-6 text-gray-700" x-show="showMenu" fill="none" stroke="currentColor"
+                            viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" x-cloak="">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M6 18L18 6M6 6l12 12"></path>
                         </svg>
                     </div>
                 </div>
             </nav>
-            <!-- Main Hero Content --> 
-            <div class="container max-w-lg px-4 py-32 mx-auto text-left md:max-w-none md:text-center">
-                <h1 class="text-5xl font-extrabold leading-10 tracking-tight text-left text-gray-900 md:text-center sm:leading-none md:text-6xl lg:text-7xl"> <span class="inline md:block">Free Event-Driven</span> <span class="relative mt-2 text-transparent bg-clip-text bg-gradient-to-br from-indigo-600 to-indigo-500 md:inline-block"> Analytics Platform</span> </h1>
-                <div class="mx-auto mt-5 text-gray-500 md:mt-12 md:max-w-lg md:text-center lg:text-lg"> Fully transparent and open-source analytics. <br> 100% free for personal and open source projects. </div>
-                <div class="flex flex-row justify-center items-center mt-12 text-center"> <span class="relative inline-flex w-full mx-2 md:w-auto"> <a href="#_" class="inline-flex items-center justify-center w-full px-4 py-3 text-base font-bold leading-6 text-indigo-600 border-indigo-600 border border-transparent rounded-full md:w-auto hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"> Learn More </a> </span> <span class="relative inline-flex w-full mx-2 md:w-auto"> <a href="#_" class="inline-flex items-center justify-center w-full px-4 py-3 text-base font-bold leading-6 text-white bg-indigo-600 border border-transparent rounded-full md:w-auto hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"> Sign Up </a> <span class="absolute top-0 right-0 px-2 py-1 -mt-3 -mr-6 text-xs font-medium leading-tight text-white bg-green-400 rounded-full">It's free*</span> </span> </div>
+            <!-- Main Hero Content -->
+            <div class="container max-w-lg px-4 py-32 lg:py-40 mx-auto text-left md:max-w-none md:text-center">
+                <h1
+                    class="text-5xl font-extrabold leading-10 tracking-tight text-left text-gray-900 md:text-center sm:leading-none md:text-6xl lg:text-7xl">
+                    <span class="inline md:block">Free Event-Driven</span> <span
+                        class="relative mt-2 text-transparent bg-clip-text bg-gradient-to-br from-indigo-600 to-indigo-500 md:inline-block">
+                        Analytics Platform</span> </h1>
+                <div class="mx-auto mt-5 text-gray-500 md:mt-12 md:max-w-lg md:text-center lg:text-lg"> Fully
+                    transparent and open-source analytics. <br> 100% free for personal and open source projects. </div>
+                <div class="flex flex-row justify-center items-center mt-12 text-center">
+                    <span class="relative inline-flex w-full mx-2 md:w-auto">
+                        <a href="#about"
+                            class="inline-flex items-center justify-center w-full px-4 py-3 text-base font-bold leading-6 text-indigo-600 border-indigo-600 border border-transparent rounded-full md:w-auto hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600">
+                            Learn More </a>
+                    </span>
+                    <span class="relative inline-flex w-full mx-2 md:w-auto">
+                        <a href="#_"
+                            class="inline-flex items-center justify-center w-full px-4 py-3 text-base font-bold leading-6 text-white bg-indigo-600 border border-transparent rounded-full md:w-auto hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600">
+                            Sign Up </a>
+                        <a href="#about-pricing"
+                            class="absolute top-0 right-0 px-2 py-1 -mt-3 -mr-6 text-xs font-medium leading-tight text-white bg-green-400 rounded-full">It's
+                            free*</a>
+                    </span>
+                </div>
             </div>
-            <!-- End Main Hero Content --> 
+            <!-- End Main Hero Content -->
         </div>
     </section>
-    <footer>
-        <div class="mx-auto text-center max-w-5xl"> <small> *The hosted OpenEvents platform is free for non-revenue generating projects such as open source projects and personal projects. Commercial projects are free while in development but may require payment after passing a free tier, as a way to cover server costs. Since OpenEvents is open-source, it can be self-hosted for free without limits. </small> </div>
-    </footer>
+
+    <!-- Section 2 -->
+    <section id="about" class="relative py-16 bg-white min-w-screen animation-fade animation-delay">
+        <div class="container px-8 mx-auto sm:px-12 xl:px-5">
+            <p
+                class="text-xs font-bold text-left text-pink-500 uppercase sm:mx-6 sm:text-center sm:text-normal sm:font-bold">
+                Got a Question? We’ve got answers.
+            </p>
+            <h3
+                class="text-2xl font-bold text-left text-gray-800 sm:text-3xl md:text-4xl lg:text-5xl sm:text-center sm:mx-0 mt-4">
+                Frequently Asked Questions
+            </h3>
+            <div
+                class="w-full px-6 py-6 mx-auto mt-10 bg-white border border-gray-200 rounded-lg sm:px-8 md:px-12 sm:py-8 sm:shadow lg:w-5/6 xl:w-2/3">
+                <h3 class="text-lg font-bold text-purple-500 sm:text-xl md:text-2xl mb-3">
+                    How does it work?
+                </h3>
+                <div class="prose max-w-fit">
+                    @markdown
+                    OpenEvents is a free, open-source analytics platform that helps you track your events and get
+                    insights into your data.
+
+                    What makes the platform diffrent is that the data you collect is open as well.
+                    All metrics tied to your account is public.
+
+                    This requires you to be transparent about the analytics data you collect.
+                    You also need to make a conscious effort to not to collect any sensitive data.
+                    I believe this helps build trust with your users.
+                    @endmarkdown
+                </div>
+            </div>
+            <div id="about-pricing"
+                class="w-full px-6 py-6 mx-auto mt-10 bg-white border border-gray-200 rounded-lg sm:px-8 md:px-12 sm:py-8 sm:shadow lg:w-5/6 xl:w-2/3">
+                <h3 class="text-lg font-bold text-purple-500 sm:text-xl md:text-2xl mb-3">
+                    What does it cost?
+                </h3>
+                <div class="prose max-w-fit">
+                    @markdown
+                    My ambition is for this service to be completly free for personal and non-revenue generating open
+                    source projects.
+
+                    I plan to make the server costs public in hopes of crowdfunding the service among those who can and
+                    want to pay.
+
+                    I may need to add storage limits for free accounts. As now now, the service is free for commercial
+                    projects as well,
+                    but in the future I think that commercial projects pay per storage/usage after hitting a generous
+                    free tier.
+
+                    Since the platform is open-source you can always self host it for free.
+                    @endmarkdown
+                </div>
+            </div>
+            <div
+                class="w-full px-6 py-6 mx-auto mt-10 bg-white border border-gray-200 rounded-lg sm:px-8 md:px-12 sm:py-8 sm:shadow lg:w-5/6 xl:w-2/3">
+                <h3 class="text-lg font-bold text-purple-500 sm:text-xl md:text-2xl mb-3">
+                    How do I get started?
+                </h3>
+                <div class="prose max-w-fit">
+                    @markdown
+                    Start by [creating an account]({{ route('register') }}). Then you need to create an API token and a
+                    project.
+
+                    > Note that currently the API tokens will give write access to all your projects.
+
+                    Once you have created your project, you can start tracking your events using your project's API
+                    endpoint.
+
+                    Some ideas of events you can track:
+
+                    - Page Views
+                    - User Signups
+                    - App Installations
+                    @endmarkdown
+                </div>
+            </div>
+            <div
+                class="w-full px-6 py-6 mx-auto mt-10 bg-white border border-gray-200 rounded-lg sm:px-8 md:px-12 sm:py-8 sm:shadow lg:w-5/6 xl:w-2/3">
+                <h3 class="text-lg font-bold text-purple-500 sm:text-xl md:text-2xl mb-3">
+                    What data can I submit?
+                </h3>
+                <div class="prose max-w-fit">
+                    @markdown
+                    Data is stored in Events. You are required to add an Event name that is under 32 characters.
+
+                    You may also add a context string up to 128 characters if you want.
+                    The context string can include additional public data.
+
+                    For example:
+                    - For a `site.view` event you way want to add the referring site.
+                    - For a `product.purchase` event you may want to add a link or identifier for the product.
+                    - For a `blog-post.created` event you can add a database reference, for example
+                    `\App\Models\Post::find(42)`
+
+                    What's important to note is that all events are public, including the context string.
+                    It is a violation of the
+                    terms of service to collect sensitive or personally identifying data.
+                    @endmarkdown
+                </div>
+            </div>
+        </div>
+    </section>
 </x-guest-layout>
