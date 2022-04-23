81ba730238c758a29db12e057de1e0f5a9a9c368
Caen De Silva <caen@desilva.se>
2022-04-23 14:50:16 +0200

    Add the controller scaffolding

---
+++ app/Http/Controllers/EventApiController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Event;
+use Illuminate\Http\Request;
+
+class EventApiController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreEventRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+}
