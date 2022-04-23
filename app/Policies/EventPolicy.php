007c574423ebc103118a7adeb44d54fbb95c8e8e
Caen De Silva <caen@desilva.se>
2022-04-23 12:59:58 +0200

    Create the Event policy

---
+++ app/Policies/EventPolicy.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Event;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class EventPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user)
+    {
+        // @todo implement rules such as request quotas
+        
+        return $user->tokenCan('event:create');
+    }
+
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Event  $event
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, Event $event)
+    {
+        return $user->id === $event->user_id
+            && $user->tokenCan('event:delete');
+    }
+}
