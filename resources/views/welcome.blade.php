81d0dff338fcdaec83a018fe0f8c9ad0fa0c4234
Caen De Silva <caen@desilva.se>
2022-04-24 10:59:48 +0200

    Tweak spacing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,16 +10,16 @@
         <div class="">
             
             <!-- Main Hero Content -->
-            <div class="container max-w-lg px-4 py-32 lg:py-40 mx-auto text-left md:max-w-none md:text-center">
+            <div class="container max-w-lg px-4 py-32 lg:py-38 mx-auto text-left md:max-w-none md:text-center">
                 <h1 class="text-5xl font-extrabold leading-10 tracking-tight text-left text-gray-900 md:text-center sm:leading-none md:text-6xl lg:text-7xl">
-                    <span class="inline md:block">Free Event-Driven</span>
-                    <span class="relative mt-2 text-transparent bg-clip-text bg-gradient-to-br from-indigo-600 to-indigo-500 md:inline-block"> Analytics Platform</span>
+                    <span class="block mb-2 sm:mb-0 text-4xl md:text-5xl lg:text-6xl">Free Event-Driven</span>
+                    <span class="relative mt-2 text-transparent bg-clip-text bg-gradient-to-br from-indigo-600 to-indigo-500 md:inline-block py-4"> Analytics Platform</span>
                 </h1>
-                <div class="mx-auto mt-5 text-gray-500 md:mt-12 md:max-w-lg md:text-center lg:text-lg">
+                <div class="mx-auto mt-5 text-gray-500 md:mt-8 md:max-w-lg md:text-center lg:text-lg">
                     Fully transparent and open-source analytics. <br />
                     100% free for personal and open source projects.
                 </div>
-                <div class="flex flex-row justify-center items-center mt-12 text-center">
+                <div class="flex flex-row justify-center items-center mt-10 text-center">
                     <span class="relative inline-flex w-full mx-2 md:w-auto">
                         <a href="#about" class="inline-flex items-center justify-center w-full px-4 py-3 text-base font-bold leading-6 text-indigo-600 border-indigo-600 border border-transparent rounded-full md:w-auto hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"> Learn More </a>
                     </span>
